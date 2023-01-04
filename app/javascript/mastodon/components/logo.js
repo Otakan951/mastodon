23a97f62630ebf387858d31c09811e17b3825b73
Otakan951 <otakan951@gmail.com>
2023-01-04 23:28:10 +0900

    [Otachon] Thie is our logo!

--- app/javascript/mastodon/components/logo.js
+++ app/javascript/mastodon/components/logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <img src="https://s3-mstdn.maud.io/logo_wordmark.png" height="32px" />
+  <img src="https://hota.hirachon.otakan.jp/system/media_attachments/files/109/631/390/341/444/688/original/3f7f590630408713.png" height="32px" />
 );
 
 export default Logo;
