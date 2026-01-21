d09107eac86270d324e0efb849277c8d150cfc26
Otakan951 <otakan951@gmail.com>
2026-01-21 12:53:47 +0000

    [Otachon] Fix dublicate language and visibility buttons

--- app/javascript/mastodon/features/compose/components/compose_form.jsx
+++ app/javascript/mastodon/features/compose/components/compose_form.jsx
@@ -261,11 +261,6 @@ class ComposeForm extends ImmutablePureComponent {
         <div className={classNames('compose-form__highlightable', { active: highlighted })} ref={this.setRef}>
           <EditIndicator />
 
-          <div className='compose-form__dropdowns'>
-            <VisibilityButton disabled={this.props.isEditing} />
-            <LanguageDropdown />
-          </div>
-
           {this.props.spoiler && (
             <div className='spoiler-input'>
               <div className='spoiler-input__border' />
